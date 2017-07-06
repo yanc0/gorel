a07fec6fc9cbff35cd6c67f3c2d19ea255fbc70b
Brice Colucci <brice.colucci@gmail.com>
2017-07-06 07:52:16 +0200

    Clean code part 1 - Rename variables with an underscore, following the camelcase convention - gofmt the file

--- gorel.go
+++ gorel.go
@@ -3,9 +3,8 @@ package main
 import (
 	"crypto/aes"
 	"crypto/cipher"
-    "encoding/base64"
+	"encoding/base64"
 	"fmt"
-	"github.com/gorilla/mux"
 	"io"
 	"io/ioutil"
 	"log"
@@ -14,13 +13,15 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
-func getHeader(r *http.Request, header string) string{
-    return r.Header[header][0]
+func getHeader(r *http.Request, header string) string {
+	return r.Header[header][0]
 }
 
-func decryptReader(token string, reader io.Reader) io.Reader{
+func decryptReader(token string, reader io.Reader) io.Reader {
 	key := []byte(token)
 	block, err := aes.NewCipher(key)
 	if err != nil {
@@ -32,7 +33,7 @@ func decryptReader(token string, reader io.Reader) io.Reader{
 	return &cipher.StreamReader{S: stream, R: reader}
 }
 
-func encryptWriter(token string, writer io.Writer) io.Writer{
+func encryptWriter(token string, writer io.Writer) io.Writer {
 	key := []byte(token)
 	block, err := aes.NewCipher(key)
 	if err != nil {
@@ -48,11 +49,11 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	dir := vars["dir"]
 	filename := vars["filename"]
-    encoded_filename:= base64.StdEncoding.EncodeToString([]byte(filename))
-    path := filepath.Join(".", dir, encoded_filename)
+	encodedFilename := base64.StdEncoding.EncodeToString([]byte(filename))
+	path := filepath.Join(".", dir, encodedFilename)
 	token := getHeader(r, "Token")
 
-    // only accept long token
+	// only accept long token
 	if len(token) != 32 {
 		http.Error(w, "Invalid Token", 400)
 		return
@@ -65,7 +66,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer f.Close()
 
-    writer := encryptWriter(token, f)
+	writer := encryptWriter(token, f)
 	if _, err := io.Copy(writer, r.Body); err != nil {
 		http.Error(w, "Error occurred copying to output stream", 500)
 		return
@@ -77,8 +78,8 @@ func downloadHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	dir := vars["dir"]
 	filename := vars["filename"]
-    encoded_filename:= base64.StdEncoding.EncodeToString([]byte(filename))
-    path := filepath.Join(".", dir, encoded_filename)
+	encodedFilename := base64.StdEncoding.EncodeToString([]byte(filename))
+	path := filepath.Join(".", dir, encodedFilename)
 	token := getHeader(r, "Token")
 
 	if len(token) != 32 {
@@ -106,7 +107,7 @@ func downloadHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", filename))
 	w.Header().Set("Connection", "close")
 
-    reader := decryptReader(token, f)
+	reader := decryptReader(token, f)
 	if _, err = io.Copy(w, reader); err != nil {
 		http.Error(w, "Error occurred copying to output stream", 500)
 		return
@@ -123,23 +124,23 @@ func downloadLatestHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-    files, _ := ioutil.ReadDir(filepath.Join("./", dir))
-    var max_mod_time int64
-    filename := ""
+	files, _ := ioutil.ReadDir(filepath.Join("./", dir))
+	var maxModTime int64
+	filename := ""
 
-    for _, file := range files {
-        fmt.Println(file.Name(), file.ModTime().Unix())
-        if file.ModTime().Unix() > max_mod_time {
-            filename = file.Name()
-            max_mod_time = file.ModTime().Unix()
-        }
-    }
+	for _, file := range files {
+		fmt.Println(file.Name(), file.ModTime().Unix())
+		if file.ModTime().Unix() > maxModTime {
+			filename = file.Name()
+			maxModTime = file.ModTime().Unix()
+		}
+	}
 
-    decoded_filename, err := base64.StdEncoding.DecodeString(filename)
-    if err != nil {
-        http.Error(w, "error decoding filename", 500)
-        return
-    }
+	decodedFilename, err := base64.StdEncoding.DecodeString(filename)
+	if err != nil {
+		http.Error(w, "error decoding filename", 500)
+		return
+	}
 
 	f, err := os.Open(filepath.Join("./", dir, filename))
 	if err != nil {
@@ -155,18 +156,19 @@ func downloadLatestHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	contentLength := uint64(fi.Size())
-	contentType := mime.TypeByExtension(filepath.Ext(string(decoded_filename)))
+	contentType := mime.TypeByExtension(filepath.Ext(string(decodedFilename)))
 	w.Header().Set("Content-Type", contentType)
 	w.Header().Set("Content-Length", strconv.FormatUint(contentLength, 10))
-	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", decoded_filename))
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", decodedFilename))
 	w.Header().Set("Connection", "close")
 
-    reader := decryptReader(token, f)
+	reader := decryptReader(token, f)
 	if _, err = io.Copy(w, reader); err != nil {
 		http.Error(w, "Error occurred copying to output stream", 500)
 		return
 	}
 }
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/{dir}/{filename}", uploadHandler).Methods("PUT")
